a730dc79cffbcb9cf36e418c39e99673c1e323d3
Justin Hurt <jhurt@connectwise.com>
2016-03-04 18:18:43 -0500

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,24 @@
 require('./less/style.less');
 require('bootstrap-webpack!../bootstrap.config.js');
 
-window.onload = resize;
-window.onresize = resize;
+const sidebarToggle = document.querySelector('.op_sidebar--toggle');
 
-function resize(){
-  document.querySelector('.oPort-Sidebar').style.height = window.innerHeight + 'px';
-  document.querySelector('.oPort-Nav').style.height = window.innerHeight + 'px';
+function toggleSidebar(e){
+  const sidebar = document.getElementById('op_l-sidebar');
+  sidebar.style.left = '-' + sidebar.offsetWidth + 'px';
+  // console.log(sidebar.style.left);
 }
+
+sidebarToggle.addEventListener('click', toggleSidebar);
+
+// function sidebar(e){
+//   console.log(e);
+// }
+
+// window.onload = resize;
+// window.onresize = resize;
+//
+// function resize(){
+//   document.querySelector('.oPort-Sidebar').style.height = window.innerHeight + 'px';
+//   document.querySelector('.oPort-Nav').style.height = window.innerHeight + 'px';
+// }
